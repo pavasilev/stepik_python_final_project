ce721a13ba50471a41204547082e2e79b9f2e035
Pavel <pavasilev1994@gmail.com>
2023-02-23 15:57:07 +0300

    change main_page and test_main_page, add login_page and locators

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        url = str(self.browser.current_url)
+        index = url.find("login")
+        print(index)
+        assert index != -1,"It is not login page!"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "There is no login form"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "There is no registration form"
